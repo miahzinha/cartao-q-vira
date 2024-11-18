6b4ce1d67a666d98d480564996e10a50a56b7da7
miahzinha <168486945+miahzinha@users.noreply.github.com>
2024-11-18 19:00:08 -0300

    ok

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
-// Função para virar os flashcards
-function flipCard(card) {
-    let front = card.querySelector('.front');
-    let back = card.querySelector('.back');
-    
-    if (front.style.display === "none") {
-        front.style.display = "block";
-        back.style.display = "none";
-    } else {
-        front.style.display = "none";
-        back.style.display = "block";
-    }
-}
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 
-// Função para checar resposta do quiz
-function checkAnswer(answer) {
-    const feedback = document.getElementById('feedback');
-    if (answer === 'Brasilia') {
-        feedback.innerText = "Correto!";
-        feedback.style.color = "green";
-    } else {
-        feedback.innerText = "Errado, tente novamente.";
-        feedback.style.color = "red";
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
     }
+    cartao.addEventListener('click', viraCartao)
+
+
+    container.appendChild(cartao)
+
 }
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)

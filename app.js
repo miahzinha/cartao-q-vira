25721272ccbaae319229993421db62ca8a7d10a9
miahzinha <168486945+miahzinha@users.noreply.github.com>
2024-11-19 18:57:09 -0300

    ok

--- app.js
+++ app.js
@@ -27,3 +27,15 @@ function criaCartao(categoria, pergunta, resposta) {
     container.appendChild(cartao)
 
 }
+</div>
+</div>
+`
+let respostaEstaVisivel = false
+function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+}
+cartao.addEventListener('click', viraCartao)
+container.appendChild(cartao)
+
+}

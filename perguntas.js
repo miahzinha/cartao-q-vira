3cc8077644b81935e6338371a61cb7d1a171b3a1
miahzinha <168486945+miahzinha@users.noreply.github.com>
2024-11-22 21:52:31 -0300

    ok

--- perguntas.js
+++ perguntas.js
@@ -1,29 +1,29 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Arcane',
+    'O que acontece com Vander?',
+    'Ele é morto no final do Ato 1 por Silco, um vilão que quer dominar Zaun. A morte de Vander é um grande ponto de virada, já que ele representava uma figura de estabilidade e esperança para os dois lados da cidade.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Arcane',
+    'Quem é Silco?',
+    'Silco é o principal vilão de Arcane. Ele é um ex-líder dos rebeldes de Zaun e agora busca dominar Zaun. Ele usa Jinx como sua principal arma e tenta manipulá-la emocionalmente, convencendo-a de que ele é a única pessoa que a entende.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Arcane',
+    'O que acontece no final do primeiro episódio?',
+    'No final do episódio 1, Vi e Jinx são forçadas a fugir de Piltover após o assalto que elas fazem, roubando objetos valiosos. A situação fica tensa e Vander tenta proteger as meninas, mas é capturado e forçado a tomar decisões difíceis. O episódio estabelece a luta entre os irmãos e as cidades de Piltover e Zaun.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Arcane',
+    'O que acontece com o Hextech no final?',
+    'No final de Arcane, a tecnologia Hextech se torna uma arma de destruição em potencial. Ela passa de uma inovação científica para algo perigoso, quando Jinx usa a tecnologia para atacar Piltover e Zaun, criando um grande conflito entre os personagens. O futuro do Hextech depende de quem controla a tecnologia.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Arcane',
+    'O que Silco faz com Jinx?',
+    'Silco manipula Jinx emocionalmente e a utiliza como sua principal arma. Ele aproveita a instabilidade emocional de Jinx e a incentiva a causar destruição em Piltover e Zaun. Ele também promete a Jinx que ele entende ela melhor do que qualquer outra pessoa, se tornando uma figura de poder para ela.'
 )
